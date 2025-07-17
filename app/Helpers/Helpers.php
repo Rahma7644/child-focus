8da58c05ccb5497645eb30693504ab1a56bcc4a0
Rahma7644 <rahma.ksh76@gmail.com>
2025-07-17 09:49:21 +0200

    edit4

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -76,7 +76,7 @@ class Helpers
               // data key should not be exist inside allOptions array's sub array
               if (!array_key_exists($data[$key], $value)) {
                 // ensure that passed value should be match with any of allOptions array value
-                $result = array_search($data[$key], $value, 'strict');
+                $result = array_search($data[$key], $value, true);
                 if (empty($result) && $result !== 0) {
                   $data[$key] = $DefaultData[$key];
                 }
